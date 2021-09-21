96680023063e12bb34692097021011b5c128c87a
Jorge Matheus <jorgenyyan@gmail.com>
2021-09-21 20:29:45 -0300

    Adicionar arquivos de projeto.

---
+++ Atividade_Faculdade2/Atividade_Faculdade2.py
@@ -0,0 +1,15 @@
+#Faça um programa com uma fução que informe se existe algu valor repetido ou se deseja adicionar elementos.
+
+números = list()
+while True:
+    n = int(input('Digite um valor:'))
+    if n not in números:
+        números.append(n)
+        print('O valor foi inserido em sua lista')
+    else:
+        print('O valor já existe na lista, portanto não será adicionado')
+    r = str(input('Deseja conitnuar? [sim/não]'))
+    if r in 'Nãonão':
+         break
+print('='*50)
+print(f'Números da sua lista: {números}')
